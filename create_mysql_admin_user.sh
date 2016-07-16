099b8d3bd9bb636aef7b59da38cf6df1100103b0
pibe81 <pliguori@gmail.com>
2016-07-16 14:19:47 +0200

    changed admin default password

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -10,7 +10,7 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+PASS=${MYSQL_PASS:-"pass"}
 _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
 echo "=> Creating MySQL admin user with ${_word} password"
 
